a16900dd2f36cd6db89abf06a4454dfc503576f2
Hsu Peng Jun <hsu.pengjun@icloud.com>
2016-06-05 10:58:44 +0800

    完成導覽列 管理區首頁直接導向至 posts.php

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Nameless Apps 收支管理系統</title>
+    <link rel="stylesheet" type="text/css" href="../semantic/semantic.css">
+    <link rel="stylesheet" type="text/css" href="../styles/style.css">
+    <script src="../scripts/jquery-2.2.2.js"></script>
+    <script src="../semantic/semantic.js"></script>
+
+    <style>
+    </style>
+</head>
+<body>
+<div class="ui container">
+    <?php
+    include 'navigation.php';
+
+    header("Location: posts.php");
+    ?>
+</div>
+</body>
+</html>
--- admin/navigation.php
+++ admin/navigation.php
@@ -13,4 +13,40 @@ $_ADMIN_NAVIGATION_CONFIGURATION = array(
     )
 );
 
+// _ADMIN_NAVIGATION_COUNT
+$ANC = count($_ADMIN_NAVIGATION_CONFIGURATION);
+
+/*
+ * 其實還有這些
+ *
+ * - 靜態圖片
+ * - 首頁輪播
+ * - 分類
+ * - 產品
+ * - 訂單
+ * - 會員
+ * - 管理者
+ */
+
+$_COLUMN_NUMBER_STYLE_CLASSES = array(
+    'zero', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine'
+);
+
+$CNSC = $_COLUMN_NUMBER_STYLE_CLASSES;
+
 ?>
+
+<div class="ui <?php echo $CNSC[$ANC]; ?> item menu">
+    <?php
+    foreach ($_ADMIN_NAVIGATION_CONFIGURATION as $item) {
+        ?>
+        <a href="<?php echo $item['href']; ?>.php" class="item">
+            <?php
+            echo $item['name'];
+            ?>
+        </a>
+        <?php
+    }
+    ?>
+</div>
