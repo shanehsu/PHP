4e3a41bb2c7fbe943d76de0c3e4a52c3b086f7f6
shanehsu <hsu.pengjun@icloud.com>
2016-06-14 17:02:49 +0800

    修正

--- util/connect.php
+++ util/connect.php
@@ -1,6 +1,6 @@
 <?php
 // $mysqli = new mysqli("php.caituglxwkil.ap-northeast-1.rds.amazonaws.com","root","12345678","group_12");
-$server = getenv('MYSQL_SERVER') == false ? 'localhost' : getenv('MYSQL_SERVER'));
+$server = getenv('MYSQL_SERVER') == false ? 'localhost' : getenv('MYSQL_SERVER');
 $mysqli = new mysqli($server, "root", "12345678", "group_12");
 
 $mysqli -> query("SET CHARACTER SET utf8");
